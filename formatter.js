57be06ec16eb752158e51fdcab19b797bdafff11
MartinGrocery <53111703+martinSCS@users.noreply.github.com>
2024-09-01 19:04:41 +0800

    Update formatter.js

--- formatter.js
+++ formatter.js
@@ -139,7 +139,7 @@ function isChinese(char) {
 }
 
 function isSpace(char) {
-    return /[ \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000]/u.test(char);
+    return /[ \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000\n]/u.test(char);
 }
 
 function isPunctuation(char) {
